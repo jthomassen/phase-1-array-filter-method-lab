0ebf0f9f9396dcdf5e97b08d8fa36322b2415c65
Jeff Thomassen <jeff.thomassen@ucdenver.edu>
2021-07-22 16:02:28 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(el => el.toUpperCase() === name.toUpperCase())
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(el => el.charAt() === name.charAt('S'))
+}
+
+function matchName(drivers, match) {
+    return drivers.filter(el => el.name === match)
+}
